2b434b1f19ee3f4b65ed28f0886a37fb827f9429
monun <monun1010@gmail.com>
2021-04-22 01:25:23 +0900

    :pencil2: Typo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,6 @@ include(
     "psychics-abilities",
     "psychics-common"
 )
-File("psychics-abilities").listFiles()?.filter { it.isDirectory && it.name != "build" }?.forEach { file ->
+file("psychics-abilities").listFiles()?.filter { it.isDirectory && it.name != "build" }?.forEach { file ->
     include(":psychics-abilities:${file.name}")
 }
