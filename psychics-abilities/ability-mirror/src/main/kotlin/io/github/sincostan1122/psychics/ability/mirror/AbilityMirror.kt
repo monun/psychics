151ee1e1d8e057083a2333f769e08bad2d856f04
sincostan1122 <youngam1122s@gmail.com>
2021-09-25 21:19:15 +0900

    수치 조정,파티클 추가

--- psychics-abilities/ability-mirror/src/main/kotlin/io/github/sincostan1122/psychics/ability/mirror/AbilityMirror.kt
+++ psychics-abilities/ability-mirror/src/main/kotlin/io/github/sincostan1122/psychics/ability/mirror/AbilityMirror.kt
@@ -29,13 +29,13 @@ class AbilityConceptMirror : AbilityConcept() {
     init {
         cost = 0.0
         durationTime = 1000L
-        cooldownTime = 1200000L
-        wand = ItemStack(Material.IRON_SWORD)
+        cooldownTime = 10000L
+        wand = ItemStack(Material.IRON_INGOT)
         displayName = "응수"
 
         description = listOf(
             text("사용 시 ${durationTime / 1000.0}초간 응수 상태가 됩니다."),
-            text("응수 상태 동안은 이동 속도가 감소되고 공격 속도가 감소합니다."),
+            text("응수 상태 동안은 이동 속도가 감소되고 공격력, 공격 속도가 감소합니다."),
             text("단, 받는 피해가 줄어들고 넉백 효과에 면역이 생깁니다."),
             text("응수 상태 동안 넉백 효과를 받을 시 잠시 공격력이 크게 증가합니다.")
         )
@@ -45,30 +45,51 @@ class AbilityMirror : ActiveAbility<AbilityConceptMirror>(), Listener {
 
     override fun onEnable() {
         psychic.registerEvents(this)
+        psychic.runTaskTimer(this::durationEffect, 0L, 1L)
     }
 
 
     override fun onCast(event: PlayerEvent, action: WandAction, target: Any?) {
+
         esper.player.addPotionEffect(
-            PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, (concept.durationTime / 50.0).toInt(), 3, false, false, false)
+            PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, (concept.durationTime / 50.0).toInt(), 5, false, false, false)
         )
         esper.player.addPotionEffect(
             PotionEffect(PotionEffectType.SLOW, (concept.durationTime / 50.0).toInt(), 5, false, false, false)
         )
         esper.player.addPotionEffect(
-            PotionEffect(PotionEffectType.SLOW_DIGGING, (concept.durationTime / 50.0).toInt(), 10, false, false, false)
+            PotionEffect(PotionEffectType.SLOW_DIGGING, (concept.durationTime / 50.0).toInt(), 20, false, false, false)
+        )
+        esper.player.addPotionEffect(
+            PotionEffect(PotionEffectType.WEAKNESS, (concept.durationTime / 50.0).toInt(), 10, false, false, false)
         )
         @EventHandler(ignoreCancelled = true)
         fun onVelocity(event: PlayerVelocityEvent) {
             if (durationTime > 0L) {
                 esper.player.removePotionEffect(PotionEffectType.SLOW_DIGGING)
+                esper.player.removePotionEffect(PotionEffectType.WEAKNESS)
                 esper.player.addPotionEffect(
-                    PotionEffect(PotionEffectType.INCREASE_DAMAGE, (concept.durationTime / 50.0).toInt(), 5, false, false, false)
+                    PotionEffect(PotionEffectType.INCREASE_DAMAGE, (concept.durationTime / 50.0).toInt(), 10, false, false, false)
                 )
                 event.isCancelled = true
+                val player = esper.player
+                val location = player.location
+                val world = location.world
+
+                world.spawnParticle(Particle.FIREWORKS_SPARK, location.x, location.y, location.z, 5, 0.25, 0.0, 0.25, 0.0, null, true)
             }
         }
+        cooldownTime = concept.cooldownTime
 
+    }
+    private fun durationEffect() {
+        if (durationTime <= 0L) return
+
+        val player = esper.player
+        val location = player.location
+        val world = location.world
+
+        world.spawnParticle(Particle.LAVA, location.x, location.y, location.z, 5, 0.25, 0.0, 0.25, 0.0, null, true)
     }
 
 
